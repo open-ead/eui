6ab57f90a5b37fb87797a7b46913a32d255be801
riall <71668864+break-core@users.noreply.github.com>
2023-08-21 18:31:53 -0400

    clang-format pass, AnimTransformBasic include fix

--- include/sys/euiAnimator.h
+++ include/sys/euiAnimator.h
@@ -1,15 +1,16 @@
 #pragma once
 
 #include <nn/font/font_Util.h>
+#include <nn/ui2d/ui2d_Animation.h>
 #include <nn/ui2d/ui2d_Pane.h>
 
 namespace nn {
 namespace ui2d {
-    class Group;
-    class AnimTransformBasic;
-    class AnimResource;
-}
-}
+class Group;
+class AnimTransformBasic;
+class AnimResource;
+} // namespace ui2d
+} // namespace nn
 
 namespace eui {
 class LayoutEx;
@@ -18,11 +19,7 @@ class Animator : public nn::ui2d::AnimTransformBasic {
 public:
     NN_RUNTIME_TYPEINFO(AnimTransformBasic);
 
-    enum PlayType {
-        PLAYTYPE_ONESHOT,
-        PLAYTYPE_LOOP,
-        PLAYTYPE_ROUNDTRIP
-    };
+    enum PlayType { PLAYTYPE_ONESHOT, PLAYTYPE_LOOP, PLAYTYPE_ROUNDTRIP };
 
     Animator();
     virtual ~Animator();
@@ -33,17 +30,17 @@ public:
     virtual void PlayRandom(PlayType type, f32 speed);
     virtual void SetEnabled(bool bEnabled);
 
-    virtual void SetupBasic(const nn::ui2d::AnimResource &animRes, eui::LayoutEx *pLayoutEx, bool enabled);
-    virtual void SetupWithGroup(const nn::ui2d::AnimResource &animRes, eui::LayoutEx *pLayoutEx, nn::ui2d::Group *pGroup, bool enabled);
-    virtual void SetupWithGroupAll(const nn::ui2d::AnimResource &animRes, eui::LayoutEx *pLayoutEx, nn::ui2d::GroupContainer *pGroupCont, bool enabled);
-    virtual void SetupWithGroupIndex(const nn::ui2d::AnimResource &animRes, eui::LayoutEx *pLayoutEx, nn::ui2d::GroupContainer *pGroupCont, u32 idx, bool enabled);
-    virtual void SetupWithPane(const nn::ui2d::AnimResource &animRes, eui::LayoutEx *pLayoutEx, nn::ui2d::Pane *pPane, bool enabled);
+    virtual void SetupBasic(const nn::ui2d::AnimResource& animRes, LayoutEx* pLayoutEx, bool enabled);
+    virtual void SetupWithGroup(const nn::ui2d::AnimResource& animRes, LayoutEx* pLayoutEx, nn::ui2d::Group* pGroup, bool enabled);
+    virtual void SetupWithGroupAll(const nn::ui2d::AnimResource& animRes, LayoutEx* pLayoutEx, nn::ui2d::GroupContainer* pGroupCont, bool enabled);
+    virtual void SetupWithGroupIndex(const nn::ui2d::AnimResource& animRes, LayoutEx* pLayoutEx, nn::ui2d::GroupContainer* pGroupCont, u32 idx, bool enabled);
+    virtual void SetupWithPane(const nn::ui2d::AnimResource& animRes, LayoutEx* pLayoutEx, nn::ui2d::Pane* pPane, bool enabled);
 
     virtual void Stop(f32 frame);
     virtual void StopAtMin();
     virtual void StopAtMax();
     virtual void StopCurrent();
-    virtual void Synchronize(const eui::Animator &animator);
+    virtual void Synchronize(const Animator& animator);
     virtual void UpdateFrame(f32 progress_frame);
 }
-}
+} // namespace eui
