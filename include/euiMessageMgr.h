0947265d0676cd3a9aeb1dd146faa5336a1030d6
Amethyst-szs <contact@amethyst.page>
2026-07-14 13:10:45 -0700

    eui: Add `MessageMgr` header (#7)

---
+++ include/euiMessageMgr.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <sead/gfx/seadColor.h>
+#include <sead/heap/seadDisposer.h>
+#include <sead/prim/seadRuntimeTypeInfo.h>
+#include <sead/prim/seadSafeString.h>
+
+namespace eui {
+
+class MessageSet;
+
+class MessageMgr {
+    SEAD_RTTI_BASE(MessageMgr);
+    SEAD_SINGLETON_DISPOSER(MessageMgr);
+
+public:
+    class Archive;
+
+public:
+    MessageMgr();
+    virtual ~MessageMgr();
+
+    virtual void loadArchive(sead::Heap*, void*, u32);
+    virtual void unloadArchive(void*);
+
+    void initialize(sead::Heap*, u32 maxColors);
+    void finalize();
+
+    MessageSet* getLayoutMessageSet(const sead::SafeString&) const;
+    MessageSet* getMessageSet(const sead::SafeString&) const;
+
+    void setGradationColor(u32 idx, sead::Color4u8 top, sead::Color4u8 bottom);
+    void setTextBoxWidthSizeOverColor(sead::Color4u8);
+
+    void dumpLastGotMessageSetInfo();
+
+protected:
+    void archiveDisposeCallback_(Archive*);
+
+private:
+    char unk[0x30];
+};
+
+static_assert(sizeof(MessageMgr) == 0x58);
+
+}  // namespace eui
